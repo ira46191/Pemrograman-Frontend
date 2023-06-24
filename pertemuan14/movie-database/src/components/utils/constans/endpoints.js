6fca6491fcb0213bb79c5ab95076cb10a8309d0f
IRA KUSUMA WARDANI <ira46191@gmail.com>
2023-06-24 16:38:25 +0700

    Update endpoints.js

--- pertemuan14/movie-database/src/components/utils/constans/endpoints.js
+++ pertemuan14/movie-database/src/components/utils/constans/endpoints.js
@@ -7,7 +7,7 @@
     TOPRATED: `${BASE_URL}/movie/top_rated?api_key=${API_KEY}`,
     RECOMMENDATIONS: (movieId) => `${BASE_URL}/movie/${movieId}/recommendations?api_key=${API_KEY}&append_to_response=videos`,
     DETAILMOVIE: (movieId) => `${BASE_URL}/movie/${movieId}?api_key=${API_KEY}&append_to_response=videos`,
-    HEROTRANDING: `${BASE_URL}/trending/all/day?api_key=${API_KEY}`,
+    HEROTRANDING: `${BASE_URL}/trending/movie/day?api_key=${API_KEY}`,
     HERODETAIL: (movieId) => `${BASE_URL}/movie/${movieId}?api_key=${API_KEY}&append_to_response=videos`,
     MOVIE: `${BASE_URL}/movie/upcoming?api_key=${API_KEY}`,
 
